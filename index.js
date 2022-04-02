5e79d1fc6db24e0a4e00a715159968b2b9aa4f7f
Frank Lee <plat4m@outlook.com>
2022-04-02 10:34:40 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const broncosWinResult = record.find(record => "W" === record.result) 
+        
+        if(broncosWinResult) {
+            return broncosWinResult.year;
+        } else {
+            return undefined
+        }
+    }
+
+superbowlWin(record);
